55385892458db7672e5a6511e8d9c5c1bbda7e8b
stefanomantova <stefano4d@gmail.com>
2022-02-04 17:59:05 -0300

    First commit

---
+++ src/Ship.js
@@ -0,0 +1,21 @@
+class Ship{
+
+constructor(length, hitPositions, isSunk){
+this.length = length;
+this.hitPositions = hitPositions;
+this.isSunk = isSunk;
+}
+
+hit(number){
+    return;
+}
+
+isSunk(){
+    return;
+}
+
+
+
+
+
+}
